37838bf6cff1975c31dbe8a4df204930226f8337
Eduardo Urrutia <urrutia.e14@gmail.com>
2017-11-28 08:53:34 -0600

    if and else excersice

---
+++ firstjsfile.js
@@ -0,0 +1,17 @@
+var price 15.00;
+var money 20.00;
+
+if(money >= price) {
+  console.log "buy the hammer"
+} else {
+  console.log "don't buy the hammer"
+};
+
+var price = 15.00;
+var money = 10.00;
+
+if(money >= price) {
+  console.log ("buy the hammer");
+} else {
+  console.log ("don't buy the hammer");
+}
